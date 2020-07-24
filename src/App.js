4574dd4cc63b8170a939981ded098005ac78713a
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-24 23:57:13 +0500

    HEADER ADDED

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './Components/header/Navigation'
 function App() {
-  return (
-    <div className="App">
+  let [menuState, setMenuState] = useState(false);
 
+  return (
+    <Router>
+    <div className="App container">
+      <Navigation/>
+            <Routes>
+        {/* <Route path="/" exact component={Home} /> */}
+        {/* <Route path="/about" exact component={Aboutus} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/gallery" exact component={Gallery} />
+        <Route path="/services" exact component={Services} /> */}
+      </Routes>
     </div>
+  </Router>
   );
 }
 
---
+++ src/Components/header/Header.js
@@ -0,0 +1,95 @@
+import React from "react";
+// import styles from './Header.module.css'
+import './Header.css'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Button } from "@material-ui/core";
+import {Link} from 'react-router-dom';
+
+
+// import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+
+    // background : '#00000000',
+    backgroundColor: 'black'
+  },
+
+  navButton : {
+    color : 'white',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    transition: 'color 0.4s linear',
+
+  '&:hover' :{
+      color : 'red'
+  },
+  
+  },
+
+  links: {
+    display: "flex",
+    // backgroundColor : 'white'
+  },
+  menuButton: {
+    color: theme.palette.primary.contrastText,
+  },
+  rightContainer: {
+    [theme.breakpoints.up("md")]: {
+      marginLeft: 80,
+    },
+  }
+}
+));
+const Header = ({ onMenuClickHandler }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="sticky" className={classes.appBar}  >
+        <Toolbar className={classes.container }>
+          {/* <Link to="/">
+          </Link> */}
+                    <a href="https://antiscamtoken.cyou/">  <img className={classes.logo} src="" alt="logo" width="80px" height='70px'/> </a>
+
+          <Hidden xsDown>
+            <div className={classes.links}>
+            {/* LINKS WILL BE PUT HERE */}
+            <Link to="Curator">   <Button className={classes.navButton}>Blogs  </Button> </Link>
+            <Link to="ListedDapps">    <Button className={classes.navButton}> About </Button> </Link>
+              
+            <Button className={classes.navButton} href="https://antiscamtoken.cyou/AST.pdf"> Contact </Button>
+
+            </div>
+          </Hidden>
+
+          <div className={classes.rightContainer}>
+
+            <Hidden smUp>
+              <IconButton
+                className={classes.menuButton}
+                edge="end"
+                aria-label="menu"
+                onClick={onMenuClickHandler}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
