104cf3fe7ed9afa65bb96896a2494850c0b6f059
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-25 20:38:03 +0500

    Second Section Started

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import FirstSection from './Sections/FirstSection/fireSection';
+import SectionSection from './Sections/SecondSection/SectionSection';
+
 const Home = () => {
     return (
+        <>
         <div>
             <FirstSection/>
         </div>
+<SectionSection/>
+        </>
     )
 }
 
---
+++ src/Components/Sections/SecondSection/SectionSection.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Typography } from '@material-ui/core';
+import '../../css/secondSection.css'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    TopHeading:{
+        color: 'white',
+    },
+    SubHeading1 : {
+        marginLeft: '280px',
+        marginRight: '280px',
+        color: 'white'
+    }
+
+}))
+const SectionSection = () => {
+    const classes = useStyles();
+    return (
+        <>
+        <section>
+        <div className="SecondHeading">
+           <Typography variant="h2" className={classes.TopHeading}>
+           Syscoin Platform
+
+           </Typography>
+
+        </div>
+
+        <div className="SecondSubHeading">
+           <Typography variant="h5" className={classes.SubHeading1}>
+           Key Features
+
+           </Typography>
+
+        </div>
+
+        </section>
+        </>
+    )
+}
+
+export default SectionSection
