80be1294499401ac0b5f508d44a2c49a2fe9ae17
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-26 15:42:06 +0500

    point of sale section done

--- src/Components/Sections/ThirdSection/PointOfSale/PointSection.js
+++ src/Components/Sections/ThirdSection/PointOfSale/PointSection.js
@@ -8,7 +8,7 @@ import '../../../css/thirdSection.css';
 const useStyles = makeStyles({
     HeadingComponent: {
         color: 'white',
-        marginTop: '25%',
+        marginTop: '10%',
         width: '50%'
 
     },
@@ -42,6 +42,16 @@ const PointSection = () => {
             The Syscoin protocol facilitates real-world 
             point-of-sale applications that can be used to transact in-store or via e-commerce. The high speed, quick resolution of asset transactions via Syscoin’s proprietary Z-DAG technology means you can empower your business with the speed and security of blockchain.    
             </Typography>
+            <br/>
+            <Typography variant="h6">
+            Some examples of retail use cases include:
+            </Typography>
+            <ul>
+                <li>De-Centralised Marketplaces and E-Commerce </li>
+                <li>Game Assets and Microtransactions</li>
+                <li>Certificate Backed Digital Goods</li>
+
+            </ul>
         </div>
 
     
