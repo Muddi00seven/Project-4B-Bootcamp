6b730d4f34b7fa423c7eca4428b99cebf94d3cc1
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-26 11:54:46 +0500

    ASSETS UPDATED

--- src/Components/Sections/SecondSection/Cards/Card.js
+++ src/Components/Sections/SecondSection/Cards/Card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Card , Grid} from '@material-ui/core';
+import {Card , Grid , CardActionArea, CardActions, CardContent,
+  CardMedia , Button , Typography} from '@material-ui/core';
+import Service1 from '../../../../assets/service/SERVICE IMAGE 2.png';
+import Service2 from '../../../../assets/service/SERVICE IMAGE4.png';
+import Service3 from '../../../../assets/service/SERVICE IMAGE7.png';
+import Service4 from '../../../../assets/service/SERVICE IMAGE11.png';
 
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     root: {
     //   maxWidth: 345,
     backgroundColor : '#00000000',
-    marginLeft: '5%'
+    marginLeft: '10%',
+    marginBottom: '10%'
 
     },
     cardContainer:{
@@ -35,7 +35,7 @@ const CardSection = () => {
           component="img"
           alt="Contemplative Reptile"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={Service1}
           title="Contemplative Reptile"
         />
         <CardContent>
@@ -66,7 +66,71 @@ const CardSection = () => {
           component="img"
           alt="Contemplative Reptile"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={Service2}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card> 
+
+      {/* down grid */}
+     </Grid>
+
+     <Grid xs={12} sm={6} md={4} item className={classes.cardContainer}>
+        <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={Service3}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card> 
+
+
+
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={Service4}
           title="Contemplative Reptile"
         />
         <CardContent>
