dcc8e34f7086632e66cee7ac47ef0072d68dfcc6
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-27 23:03:52 +0500

    Update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,18 +2,24 @@ import React from 'react'
 import FirstSection from './Sections/FirstSection/fireSection';
 import SectionSection from './Sections/SecondSection/SectionSection';
 import ThirdSection from './Sections/ThirdSection/ThirdSection'
+import Contact from './contact/Contact'
 const Home = () => {
     return (
         <>
-        <div>
-            <FirstSection/>
-        </div>
-        <div>
-<SectionSection/>
-</div>
-<section>   
-     <ThirdSection/>
-</section>
+    <section>
+        <FirstSection/>
+    </section>
+
+    <section>
+        <SectionSection/>
+    </section>
+    <section>   
+         <ThirdSection/>
+    </section>
+    <div>
+        <Contact/>
+    </div>
+
   
         </>
     )
